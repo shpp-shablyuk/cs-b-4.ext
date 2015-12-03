ecd43f403d48ead895ac7371391b035eb4c7da85
bulld0zer <sergey.khablyuk@gmail.com>
2015-12-03 17:40:17 +0200

    fix

--- src/tree.cpp
+++ src/tree.cpp
@@ -4,40 +4,36 @@
 #include <cmath>
 using namespace std;
 
+const int ORDER = 6;
 const double SIZE = 100;
-const int ORDER = 7;
-const double REDUCTION_RATIO = 0.7;
-const string TRUNC_COLOR = "#926239";
+const double REDUCTION_RATIO = 0.6;
+const int WINDOW_SIZE = 600;
+const int TILT_BRANCH = 45;
+const int TILT_TREE = 90; //  tilt a tree trunk
 
 void drawTree(GWindow & gw, GPoint pt, double r, double theta, int order);
 
 int main() {
-    GWindow gw(800, 800);
-    gw.setColor(TRUNC_COLOR);
+    GWindow gw(WINDOW_SIZE, WINDOW_SIZE);
 
     double cx = gw.getWidth() / 2;
     double yWindowHeight = gw.getHeight();
 
-    GPoint pt(cx, yWindowHeight * 0.7);
-    gw.drawLine(cx, yWindowHeight, cx, yWindowHeight * 0.7);
-    drawTree(gw, pt, SIZE, 90, ORDER);
+    GPoint pt(cx, yWindowHeight * REDUCTION_RATIO);
+    //  to draw a tree trunk
+    gw.drawLine(cx, yWindowHeight, cx, yWindowHeight * REDUCTION_RATIO);
+    drawTree(gw, pt, SIZE, TILT_TREE, ORDER);
 
 	return 0;
 }
 
 void drawTree(GWindow & gw, GPoint pt, double r, double theta, int order) {
     if (order != 0) {
-        if (order < 4) {
-            gw.setColor("GREEN");
-        } else {
-            gw.setColor(TRUNC_COLOR);
-        }
-        GPoint pt1 = gw.drawPolarLine(pt, r, theta - 45);
-        GPoint pt2 = gw.drawPolarLine(pt, r, theta);
-        GPoint pt3 = gw.drawPolarLine(pt, r, theta + 45);
+        GPoint pt1 = gw.drawPolarLine(pt, r, theta - TILT_BRANCH);
+        drawTree(gw, pt1, r * REDUCTION_RATIO, theta - TILT_BRANCH, order - 1);
 
-        drawTree(gw, pt1, r * REDUCTION_RATIO, theta - 45, order - 1);
-        drawTree(gw, pt2, r * REDUCTION_RATIO, theta, order - 1);
-        drawTree(gw, pt3, r * REDUCTION_RATIO, theta + 45, order - 1);
+        GPoint pt3 = gw.drawPolarLine(pt, r, theta + TILT_BRANCH);
+        drawTree(gw, pt3, r * REDUCTION_RATIO, theta + TILT_BRANCH, order - 1);
+
     }
 }
